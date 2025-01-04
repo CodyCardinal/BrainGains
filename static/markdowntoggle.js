79494fde765c94d08584bcb42a6c942dab8067a8
Cody Cardinal <cardinalc@gmail.com>
2025-01-04 15:33:05 -0800

    redesign: layout for edit and create fix: modern javascript in markdowntoggle script.

--- static/markdowntoggle.js
+++ static/markdowntoggle.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   let tabEnabled = true;
 
   function enableTab(id) {
-    var el = document.getElementById(id);
+    const el = document.getElementById(id);
     el.addEventListener("keydown", function (e) {
       if (tabEnabled && e.key === "Tab" && e.shiftKey) {
         e.preventDefault();
-        var start = this.selectionStart;
-        var end = this.selectionEnd;
-        this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);
+        const start         = this.selectionStart;
+        const end           = this.selectionEnd;
+        this.value          = this.value.substring(0, start) + "\t" + this.value.substring(end);
         this.selectionStart = this.selectionEnd = start + 1;
       }
     });
@@ -19,14 +19,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   document.getElementById("toggle-tab").addEventListener("click", function (e) {
     e.preventDefault();
-    tabEnabled = !tabEnabled;
-    const icon = document.getElementById("toggle-icon");
+    tabEnabled   = !tabEnabled;
+    const icon   = document.getElementById("toggle-icon");
+    const button = document.getElementById("toggle-tab");
     if (tabEnabled) {
       icon.classList.remove("bi-toggle-off");
       icon.classList.add("bi-toggle-on");
+      button.classList.remove("btn-outline-secondary");
+      button.classList.add("btn-outline-success");
     } else {
       icon.classList.remove("bi-toggle-on");
       icon.classList.add("bi-toggle-off");
+      button.classList.remove("btn-outline-success");
+      button.classList.add("btn-outline-secondary");
     }
   });
 });
